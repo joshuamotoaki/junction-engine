23a7dd3839f9d651a3250a0a0d82c4d9cbc103d0
Joshua Lau <dachshunduniverse@gmail.com>
2025-08-13 12:00:05 -0700

    It's svelte now

--- apps/web/app/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const GITHUB_URL = "https://github.com/joshuamotoaki/junction-engine";
-export const RELEASE_YEAR = "2023";
--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,61 +1,44 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import typescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
 
-export default [
-    {
-        ignores: ["build/**", "public/**", "*.config.js", ".react-router/**"],
-    },
-    js.configs.recommended,
-    {
-        files: ["**/*.{js,jsx,ts,tsx}"],
-        plugins: {
-            react,
-            "react-hooks": reactHooks,
-            "@typescript-eslint": typescript,
-            prettier,
-        },
-        languageOptions: {
-            parser: typescriptParser,
-            ecmaVersion: 2022,
-            sourceType: "module",
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-                project: "./tsconfig.json",
-            },
-            globals: {
-                window: "readonly",
-                document: "readonly",
-                console: "readonly",
-                process: "readonly",
-                localStorage: "readonly",
-                sessionStorage: "readonly",
-                fetch: "readonly",
-                React: "readonly",
-            },
-        },
-        rules: {
-            ...react.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            ...typescript.configs.recommended.rules,
-            "react/react-in-jsx-scope": "off",
-            "react/prop-types": "off",
-            "@typescript-eslint/no-unused-vars": "warn",
-            "@typescript-eslint/explicit-function-return-type": "off",
-            "prettier/prettier": "error",
-            "no-console": "warn",
-        },
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-    },
-    prettierConfig,
-];
+import prettier from 'eslint-config-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node }
+		},
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
+		}
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
+		}
+	},
+	storybook.configs['flat/recommended']
+);
